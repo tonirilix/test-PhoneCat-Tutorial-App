0cf91a1179b627b61d97a5d67f05d37839ca7a90
tonirilix <jesusmontes.mx@hotmail.com>
2015-03-23 09:57:54 -0400

    Adding Event Handlers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,11 +15,21 @@ phonecatControllers.controller('PhoneListCtrl',['$scope', '$http',
 			$scope.phones = data;
 		});
 	  $scope.orderProp = 'age';
+
 }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http', 
 	function($scope, $routeParams, $http){
 		$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
       $scope.phone = data;
+      $scope.mainImageUrl = data.images[0];
     });
+
+    $scope.setImage = function(imageUrl){
+    	$scope.mainImageUrl = imageUrl;
+    };
+
+    $scope.hello = function(name){
+    	alert('Hello ' + (name || 'world') + '!');
+    };
 }])
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -71,15 +71,29 @@ describe('PhoneCat App', function() {
 
 	describe('Phone detail view', function() {
 
-    beforeEach(function() {
-      browser.get('app/index.html#/phones/nexus-s');
-    });
+		beforeEach(function() {
+			browser.get('app/index.html#/phones/nexus-s');
+		});
 
 
-    it('should display nexus-s page', function() {
-      expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
-    });
-  });
+		it('should display nexus-s page', function() {
+			expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
+		});
+
+		it('should display the first phone image as the main phone image', function() {
+			expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+		});
+
+
+		it('should swap main image if a thumbnail image is clicked on', function() {
+			element(by.css('.phone-thumbs li:nth-child(3) img')).click();
+			expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
+
+			element(by.css('.phone-thumbs li:nth-child(1) img')).click();
+			expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+		});
+
+	});
 
 });
 

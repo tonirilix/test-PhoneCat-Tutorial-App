bf1a248f7ee76c23c1359684d958d9e704288e61
tonirilix <jesusmontes.mx@hotmail.com>
2015-03-22 22:59:03 -0400

    Angular Templates - Adding some dynamic content

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,22 @@
 'use strict';
 
 /* Controllers */
+/**
+* phonecatApp Module;
+*
+* Description
+*/
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope, $http){
+	$scope.name = "World";
+	$scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+
+})
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,8 +4,18 @@
 
 describe('controllers', function() {
 
-  it("should do something", function() {
+	describe('PhoneListCtrl', function(){
 
-  });
+		beforeEach(module('phonecatApp'));
+
+		it('should create "phones" model with 3 phones', inject(function($controller) {
+			var scope = {},
+			ctrl = $controller('PhoneListCtrl', {$scope:scope});
+
+			expect(scope.phones.length).toBe(3);			
+			expect(scope.name).toBe('World'); 
+		}));		
+
+	});
 
 });

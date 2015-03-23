eea19e3baaa206ddfea67b2eb4df8aaedb725d10
tonirilix <jesusmontes.mx@hotmail.com>
2015-03-23 00:27:32 -0400

    Adding XHRs & Dependency Injection

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,19 +8,12 @@
 */
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http){
+phonecatApp.controller('PhoneListCtrl',['$scope', '$http',
+	function($scope, $http){
 	$scope.name = "World";
-	$scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age': 1},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 2},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 3}
-  ];
+	$http.get('phones/phones.json').success(function(data){
+		$scope.phones = data;
+	});
   $scope.orderProp = 'age';
 
-})
+}])
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,23 +3,36 @@
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
-  describe('PhoneListCtrl', function(){
-    var scope, ctrl;
+	describe('PhoneListCtrl', function(){
+		var scope, ctrl, $httpBackend;
 
-    beforeEach(module('phonecatApp'));
+	  // Load our app module definition before each test.
+	  beforeEach(module('phonecatApp'));
 
-    beforeEach(inject(function($controller) {
-      scope = {};
-      ctrl = $controller('PhoneListCtrl', {$scope:scope});
-    }));
+	  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+	  // This allows us to inject a service but then attach it to a variable
+	  // with the same name as the service in order to avoid a name conflict.
+	  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+	  	$httpBackend = _$httpBackend_;
+	  	$httpBackend.expectGET('phones/phones.json').
+	  	respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-    it('should create "phones" model with 3 phones', function() {
-      expect(scope.phones.length).toBe(3);
-    });
+	  	scope = $rootScope.$new();
+	  	ctrl = $controller('PhoneListCtrl', {$scope: scope});
+	  }));
+
+	  it('should create "phones" model with 2 phones fetched from xhr', function() {
+	  	expect(scope.phones).toBeUndefined();
+	  	$httpBackend.flush();
+
+	  	expect(scope.phones).toEqual([{name: 'Nexus S'},
+	  		{name: 'Motorola DROID'}]);
+	  });
+
+	  it('should set the default value of orderProp model', function() {
+	  	expect(scope.orderProp).toBe('age');
+	  });
 
 
-    it('should set the default value of orderProp model', function() {
-      expect(scope.orderProp).toBe('age');
-    });
-  });
+	});
 });
